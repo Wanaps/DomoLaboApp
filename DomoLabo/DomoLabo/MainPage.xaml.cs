17b07cd6c84f611cee90d03a6228958ad294ccf7
LeoM <54435611+Tenlutshy@users.noreply.github.com>
2023-11-29 11:43:03 +0100

    Update

---
+++ DomoLabo/DomoLabo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DomoLabo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+        {
+            Console.WriteLine("nique ta mere la pute de ta race de ta mere la pute");
+        }
+
+
+        private void StationPage_OnTapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StationPage());
+        }
+    }
+}
---
+++ DomoLabo/DomoLabo/VentilationPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DomoLabo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VentilationPage : ContentPage
+    {
+        public VentilationPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
