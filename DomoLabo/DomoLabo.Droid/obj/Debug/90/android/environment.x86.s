1dc221f91bb95ffac091f37d37f395fd9047793f
Mattéo Verdez <matteo.verdez@ynov.com>
2023-11-29 09:48:20 +0100

    back button new visual

--- DomoLabo/DomoLabo.Droid/obj/Debug/90/android/environment.x86.s
+++ DomoLabo/DomoLabo.Droid/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"f36fcd44-937b-45b1-b34c-b1f8599e94c1"
+	.asciz	"a7439cb1-b116-4b4d-8678-4cdf21d27cb5"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- DomoLabo/DomoLabo/App.xaml.cs
+++ DomoLabo/DomoLabo/App.xaml.cs
@@ -11,7 +11,7 @@ namespace DomoLabo
         public App()
         {
             InitializeComponent();
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
